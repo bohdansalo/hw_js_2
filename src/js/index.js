7f20d049c0a5d045fe947fb5980b9266a0b25d19
Богдан <bohdansalo31@gmail.com>
2025-01-28 18:08:27 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+// // ==== 1
+// const celsius = 12;
+// const fahrenheit = celsius * 2 + 15;
+
+// console.log("Fahrenheit: " + fahrenheit);
+// // ==== 2
+
+// const daysMonths = 30;
+// const hoursMonths = daysMonths * 24;
+// const minMonths = 60 * daysMonths * daysMonths;
+
+// console.log("Days: " + daysMonths + " days");
+// console.log("Hours: " + hoursMonths + " hours");
+// console.log("Minutes: " + minMonths + " min");
+
+// ==== 3
+
+let health = 100;
+let energy = 100;
+
+console.log("Залишилось здоров'я: " + (health - 55) + " hp");
+console.log("Залишилось енергії: " + (energy - 35) + " energy");
