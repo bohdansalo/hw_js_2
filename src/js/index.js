804ab821b4aa4c47ee9ff089788188da22496718
Богдан <bohdansalo31@gmail.com>
2025-02-17 22:38:29 +0200

    completed

--- src/js/index.js
+++ src/js/index.js
@@ -15,8 +15,40 @@
 
 // ==== 3
 
-let health = 100;
-let energy = 100;
+// let health = 100;
+// let energy = 100;
 
-console.log("Залишилось здоров'я: " + (health - 55) + " hp");
-console.log("Залишилось енергії: " + (energy - 35) + " energy");
+// console.log("Залишилось здоров'я: " + (health - 55) + " hp");
+// console.log("Залишилось енергії: " + (energy - 35) + " energy");
+
+// ==== 4
+// const price = 500;
+// const discount = 10;
+
+// const lastPrice = price - (price * discount) / 100;
+
+// console.log(lastPrice);
+
+// ==== 5
+// const number = 5.55;
+// const circleNumber = Math.floor(number);
+
+// console.log(circleNumber);
+
+// ==== 6
+
+// const number = "15.55";
+// const floatNumber = parseFloat(number);
+
+// console.log(floatNumber);
+
+// ==== 7
+// const strNumber = "505";
+// const number = parseInt(strNumber);
+// console.log(number);
+
+// ==== 8
+// const number = 400;
+// const root = Math.sqrt(number);
+
+// console.log(root);
